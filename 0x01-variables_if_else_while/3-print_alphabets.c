fa181a0740e091c1fc6d22fad7b0b1ea09d37fa0
akohinmiogbegbile <akohinmi90@gmail.com>
2023-02-17 18:06:36 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+	       putchar(i);
+	putchar('\n');
+	return (0);
+}
